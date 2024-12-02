acc259b655e302fd42c9fd61ab3348f583fc1acb
Sinitsa <sinitsammi@gmail.com>
2024-12-02 15:23:07 +0300

    work on bugs

--- CrystalShop/Program.cs
+++ CrystalShop/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CrystalShop
 {
@@ -12,21 +9,21 @@ namespace CrystalShop
         {
             Console.OutputEncoding = Encoding.Unicode;
             
-            int crystal = 0;
+            int crystals = 0;
             int gold;
             int priseOfCrystal = 10;
-            int numberOfCrystals;
+            int crystalsInOrder;
 
             Console.WriteLine("Приветствуем Вас в магазине кристаллов");
             Console.Write("Сколько золота у Вас есть? ");
             gold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Сколько кристаллов хотите приобрести? ");
-            numberOfCrystals = Convert.ToInt32(Console.ReadLine());
+            crystalsInOrder = Convert.ToInt32(Console.ReadLine());
 
-            gold -= priseOfCrystal * numberOfCrystals;
-            crystal += numberOfCrystals;
+            gold -= priseOfCrystal * crystalsInOrder;
+            crystals += crystalsInOrder;
 
-            Console.WriteLine($"У вас {crystal} кристаллов и {gold} золота");
+            Console.WriteLine($"У вас {crystals} кристаллов и {gold} золота");
         }
     }
 }
